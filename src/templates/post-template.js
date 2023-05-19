fa1a362c5e754d0a4c0ddb5f485e4277b077129b
Stuart Mackenzie <ilovediscostu@gmail.com>
2023-05-19 17:42:58 +0100

    update

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Link, graphql } from 'gatsby'
-import { FaTwitter, FaFacebook } from 'react-icons/fa'
-import { MdEmail } from 'react-icons/md'
+/* import { FaTwitter, FaFacebook } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md' */
 import { EpisodeSizer, EpisodeDuration } from '../helpers/helper.js'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
@@ -16,9 +16,9 @@ const PostTemplate = ({ data, children }) => {
   const episodeTitle = `${post.frontmatter.episodeNumber}: ${post.frontmatter.title}`
   const episodeSize = EpisodeSizer(post.frontmatter.episodeBytes, 2)
   const episodeLength = EpisodeDuration(post.frontmatter.episodeSeconds)
-  const facebookShare = `http://www.facebook.com/sharer.php?u=${siteUrl}/memos${post.fields.slug}`
+  /* const facebookShare = `http://www.facebook.com/sharer.php?u=${siteUrl}/memos${post.fields.slug}`
   const twitterShare = `https://twitter.com/intent/tweet?text=${episodeTitle} - ${siteUrl}/memos${post.fields.slug}`
-  const emailShare = `mailto:?subject=Check out this podcast episode site&body=${episodeTitle} - ${siteUrl}/memos${post.fields.slug}`
+  const emailShare = `mailto:?subject=Check out this podcast episode site&body=${episodeTitle} - ${siteUrl}/memos${post.fields.slug}` */
   const { previous, next } = data
   return (
     <Layout>
@@ -53,7 +53,7 @@ const PostTemplate = ({ data, children }) => {
       </audio>
 
       <div className="content mt-4">{children}</div>
-      <p className="is-uppercase is-size-7 mb-2">SHARE THIS MEMO</p>
+      {/* <p className="is-uppercase is-size-7 mb-2">SHARE THIS MEMO</p>
       <div className="is-aligned-left">
         <a className="mr-4" href={twitterShare} title="share on twitter">
           <FaTwitter size="2rem" />
@@ -64,7 +64,7 @@ const PostTemplate = ({ data, children }) => {
         <a className="mr-4" href={emailShare} title="share via email">
           <MdEmail size="2rem" />
         </a>
-      </div>
+      </div> */}
       <Embed websiteId="8919" id={post.fields.slug} />
       <nav
         className="pagination py-4"
